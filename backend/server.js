db79fce4e12a9c55242822e9d075d622f6ae0eb8
Steven F <stevenofficial9798@gmail.com>
2026-03-11 19:34:32 +0530

    MongoDB Atlas and Redis Cloud connection done.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ let server
 const startServer = async () => {
   try {
     await connectDB()
-    // await connectRedis()
+    await connectRedis()
 
     server = app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
